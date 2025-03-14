59b587a29f372f69ad9bb05d916572865ec58bef
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-14 15:03:45 +0530

    create repos creation script

---
+++ ecr-repos-creation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# List of repository names
+PROJECT_NAME=instana
+REPOS=("cart" "user" "shipping" "payment" "frontend" "mongo" "mysql" "rabbit")
+
+# Loop through each repo name and create it in ECR
+for REPO in "${REPOS[@]}"; do
+    echo "Creating ECR repository: $REPO"
+    aws ecr create-repository --repository-name "$PROJECT_NAME/$REPO"
+    if [ $? -eq 0 ]; then
+        echo "Successfully created: $PROJECT_NAME/$REPO"
+    else
+        echo "Failed to create: $PROJECT_NAME/$REPO"
+    fi
+done
